d0e8dd1814814a00382a9d43af35fcdaa5c8c137
mohammed kabir <kbshowkb@gmail.com>
2020-12-13 07:36:56 -1200

    Updates

---
+++ precache-manifest.31d9109a7273efea39c70301d95331b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b1eae567bff9bdf32f20b908204459",
+    "url": "/react-conquer-task/index.html"
+  },
+  {
+    "revision": "367f8a1b53183c350d91",
+    "url": "/react-conquer-task/static/css/main.c8b39ddb.chunk.css"
+  },
+  {
+    "revision": "803b049da03048d7c0f6",
+    "url": "/react-conquer-task/static/js/2.3eacfe40.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-conquer-task/static/js/2.3eacfe40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367f8a1b53183c350d91",
+    "url": "/react-conquer-task/static/js/main.8b1fb24c.chunk.js"
+  },
+  {
+    "revision": "372bca9dabdaa3c67841",
+    "url": "/react-conquer-task/static/js/runtime-main.f805c921.js"
+  },
+  {
+    "revision": "d1e21ce0ad1520748350c03c302d4ac6",
+    "url": "/react-conquer-task/static/media/01.d1e21ce0.jpg"
+  },
+  {
+    "revision": "4509fa02b18631c12cd38fda36fcf814",
+    "url": "/react-conquer-task/static/media/02.4509fa02.jpg"
+  },
+  {
+    "revision": "614cb30964a7d55827169ec986bd7a9b",
+    "url": "/react-conquer-task/static/media/03.614cb309.jpg"
+  },
+  {
+    "revision": "a6df4c56b52cc3df91567501463d5884",
+    "url": "/react-conquer-task/static/media/04.a6df4c56.jpg"
+  },
+  {
+    "revision": "c64cc6149787304daead8ad54bd7c54a",
+    "url": "/react-conquer-task/static/media/05.c64cc614.jpg"
+  },
+  {
+    "revision": "abec5727359db1989dbd021f6e3bb6cc",
+    "url": "/react-conquer-task/static/media/06.abec5727.jpg"
+  },
+  {
+    "revision": "669bd8cff7ed5acaa118e319692fcc46",
+    "url": "/react-conquer-task/static/media/07.669bd8cf.jpg"
+  },
+  {
+    "revision": "6089d991323d4544ad6e2292e3e70380",
+    "url": "/react-conquer-task/static/media/08.6089d991.jpg"
+  },
+  {
+    "revision": "fa66fb8170e18cca7280483b5fa83795",
+    "url": "/react-conquer-task/static/media/09.fa66fb81.jpg"
+  },
+  {
+    "revision": "a069e7598e8bed496a5037ebfc85169e",
+    "url": "/react-conquer-task/static/media/10.a069e759.jpg"
+  },
+  {
+    "revision": "4c5500a77121464eca9cf7b95c18445d",
+    "url": "/react-conquer-task/static/media/11.4c5500a7.jpg"
+  },
+  {
+    "revision": "c2292cce1e2b3dfc123bf31c3dbda60e",
+    "url": "/react-conquer-task/static/media/12.c2292cce.jpg"
+  },
+  {
+    "revision": "cee6d1c1d348cdae061ed60ec8562f91",
+    "url": "/react-conquer-task/static/media/13.cee6d1c1.jpg"
+  }
+]);
